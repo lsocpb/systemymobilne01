27765e801c0bb6da30bb4d3adab973f2bbbc6df0
lsocpb <lukasssonik@gmail.com>
2023-10-05 16:43:13 +0200

    questions added +answercorrectness method added

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button trueBtn, falseBtn, nextBtn;
+    private TextView questionTextView;
+
+    private int currentIndex = 0;
+    private Question[] questions = new Question[] {
+            new Question(R.string.q_activity, true),
+            new Question(R.string.q_version, false),
+            new Question(R.string.q_listener, true),
+            new Question(R.string.q_resources, true),
+            new Question(R.string.q_find_resources, false)
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void checkAnswerCorrectness(boolean userAnswer) {
+        boolean correctAnswer = questions[currentIndex].isTrueAnswer();
+        int messageId;
+        if (userAnswer == correctAnswer)
+            messageId = R.string.correct_answer;
+        else
+            messageId = R.string.incorrect_answer;
+        Toast.makeText(MainActivity.this, messageId, Toast.LENGTH_SHORT).show();
     }
 }
