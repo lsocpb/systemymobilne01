b61e1ed10d062d7316c4cab3870ea0aa1b6e96bd
lsocpb <lukasssonik@gmail.com>
2023-10-13 14:39:17 +0200

    life cycle methods added

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -32,11 +33,11 @@ public class MainActivity extends AppCompatActivity {
         trueBtn = findViewById(R.id.true_button);
         falseBtn = findViewById(R.id.false_button);
         nextBtn = findViewById(R.id.next_button);
+        scoreTextView = findViewById(R.id.score_text_view);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setTitle("Quiz");
         toolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
-        scoreTextView = findViewById(R.id.score_text_view);
 
         trueBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -80,4 +81,33 @@ public class MainActivity extends AppCompatActivity {
         scoreTextView.setText("Score: " + score);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("QUIZ_TAG", "Wywołanie onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("QUIZ_TAG", "Wywołanie onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("QUIZ_TAG", "Wywołanie onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("QUIZ_TAG", "Wywołanie onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("QUIZ_TAG", "Wywołanie onResume");
+    }
 }
